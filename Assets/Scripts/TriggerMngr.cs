4583c2ebea1191383ef18e694dcfa7b9a00da3d1
jnxnpn <>
2019-07-14 10:04:42 +0800

    initial commit

---
+++ Assets/Scripts/TriggerMngr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+
+public class TriggerMngr : MonoBehaviour
+{
+
+    public Canvas myCanvas; //Your target for the refference
+
+    void Awake(){
+        myCanvas.enabled = false;
+    }
+    void OnTriggerExit(){
+        myCanvas.enabled = false;
+    }
+    
+    //If you want to be more specific to what gets enabled and store it all in one script you can check tags
+    
+    void OnTriggerEnter(Collider collider){
+        print("collision");
+        if (collider.gameObject.CompareTag("duck")){
+            myCanvas.enabled = true;
+        }
+    }
+    
+}
